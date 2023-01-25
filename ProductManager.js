7008b28bb4d2b1b2b8740e83cec902fdff55575e
Nicolas <nicolasricciardi24@gmail.com>
2023-01-24 22:37:07 -0300

    Primer desafio terminado

--- ProductManager.js
+++ ProductManager.js
@@ -1,5 +1,9 @@
 class product{
+
+    static id = 0;
+
     constructor(nombre, desc, precio, img, codigo, stock){
+        this.id = ++product.id
         this.nombre = nombre;
         this.desc = desc;
         this.precio = precio;
@@ -7,27 +11,38 @@ class product{
         this.codigo = codigo;
         this.stock = stock
     }    
-}
+};
 
 class productManager{
     constructor(){
-        this.product = new Array();
+        this.products = new Array();
+    }
+    
+    getProducts() {
+        return this.products;
     }
 
-    addProduct(product) {
-        if (this.product.find(element => element.code === product.code)) {
-            return console.log("Producto ya esxiste")
+    addProduct(nombre, desc, precio, img, codigo, stock) {
+        let nuevoProduct = new product(nombre, desc, precio, img, codigo, stock)
+        if (this.products.find(element => element.codigo === nuevoProduct.codigo)) {
+            return console.log("Producto ya existe")
         } else {
-            this.product.push(product);
+            this.products.push(nuevoProduct);
             return console.log("Producto agregado")
         }
     }
 
-    getAllProducts() {
-        return this.product
-    }
-
     getProductById(id) {
-        return this.product.find(element => element.code === id) || "Ese producto noi existe"
+        return this.products.find(element => element.id === id) || "Ese producto no existe"
     }
 }
+let ProductManager = new productManager();
+
+//test
+
+/* console.log(ProductManager); 
+ProductManager.addProduct("joystick", "simulador", "$5000", "sin img", 1020, "20");
+ProductManager.addProduct("joystick2", "juego", "$3000", "sin img", 1021, "25");
+console.log(ProductManager);
+console.log(ProductManager.getProductById(2));
+console.log(ProductManager.getProducts());  */
